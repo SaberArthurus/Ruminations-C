f478041bf6be369f6302e3d7f8395b156d38c16e
Sci.Gao <SaberArthurus@gmail.com>
2017-05-05 11:42:38 +0800

    add glog

---
+++ glog/main.cpp
@@ -0,0 +1,15 @@
+#include <glog/logging.h>  
+  
+int main(int argc,char* argv[]) {  
+    // If glog is used to parse the command line   
+    // google::ParseCommandLineFlags(&argc, &argv, true);  
+  
+    // Initialize Google's logging library.  
+    google::InitGoogleLogging(argv[0]);  
+  
+    FLAGS_log_dir = "./log";  
+  
+    LOG(INFO) << "hello world";  
+  
+    return 0;  
+}  
